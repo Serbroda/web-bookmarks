15b7cb88628ba36be03b301c782295fe18995e88
Danny Rottstegge <danny@rottstegge.net>
2022-11-08 23:40:35 +0100

    🚧 Add typings for useMobileNavbar

--- frontend/src/stores/navigation.ts
+++ frontend/src/stores/navigation.ts
@@ -1,6 +1,11 @@
 import create from 'zustand'
 
-const useMobileNavbar = create((set) => ({
+export interface UseMovileNavbarState {
+    isOpen: boolean;
+    setOpen: (value: boolean) => void;
+}
+
+const useMobileNavbar = create<UseMovileNavbarState>((set) => ({
     isOpen: true,
     setOpen: (value: boolean) => set({isOpen: value}),
   }));
