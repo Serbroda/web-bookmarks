fd6a9445a709f3e2b43b54860f09755d8c1e2d11
Danny Rottstegge <danny@rottstegge.net>
2022-11-11 16:19:28 +0100

    💄 Add custom button styles

--- frontend/src/routes/GroupPage.tsx
+++ frontend/src/routes/GroupPage.tsx
@@ -11,6 +11,12 @@ const GroupPage = () => {
       <div className="w-64 h-64 bg-green-500 r horizontal rounded-md">Test</div>
 
       <MyModal />
+
+      <span className="btn-group">
+        <button className="btn btn-xs">xs</button>
+        <button className="btn btn-primary btn-xs">xs</button>
+        <button className="btn btn-primary btn-xs">xs</button>
+      </span>
     </>
   );
 };
