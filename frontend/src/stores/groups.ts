bafc21d6f0131aded5d0231215bc5a957d634f0c
Danny Rottstegge <danny@rottstegge.net>
2023-07-14 13:55:31 +0200

    clean everything

--- frontend/src/stores/groups.ts
+++
@@ -1,6 +0,0 @@
-import type { GroupSubscriptionDto } from "../models/GroupSubscriptionDto";
-import { writable } from "svelte/store";
-import type { GroupDto } from "../models/GroupDto";
-
-export const groups = writable<GroupDto[]>([]);
-export const groupSubscriptions = writable<GroupSubscriptionDto[]>([]);
