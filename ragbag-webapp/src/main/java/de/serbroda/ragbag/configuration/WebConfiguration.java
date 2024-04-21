0d19f356414233e7a4e73f08ad700468436566b7
Danny Rottstegge <danny@rottstegge.net>
2024-04-21 23:52:17 +0200

    modularize project

---
+++ ragbag-webapp/src/main/java/de/serbroda/ragbag/configuration/WebConfiguration.java
@@ -0,0 +1,10 @@
+package de.serbroda.ragbag.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+
+}
