dd1672d1ce18503cb015971dc9753c6c8a797d21
Danny Rottstegge <danny@rottstegge.net>
2024-04-21 23:10:59 +0200

    first in java

---
+++ src/test/java/de/serbroda/ragbag/RagbagApplicationTests.java
@@ -0,0 +1,13 @@
+package de.serbroda.ragbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagbagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
