57b0bc3208bcb8f3dc943db302ef5aae66dd2b12
Danny Rottstegge <danny@rottstegge.net>
2024-08-27 23:09:17 +0200

    add me endpoint

--- ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/AuthenticationController.java
+++ ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//https://medium.com/@tericcabrel/implement-jwt-authentication-in-a-spring-boot-3-application-5839e4fd8fac
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
--- ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/v1/UserController.java
+++ ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/v1/UserController.java
@@ -1,19 +1,22 @@
 package de.serbroda.ragbag.controller.v1;
 
-import de.serbroda.ragbag.controller.v1.api.UserApi;
 import de.serbroda.ragbag.dtos.UserDto;
 import de.serbroda.ragbag.mappers.UserMapper;
 import de.serbroda.ragbag.models.Account;
 import de.serbroda.ragbag.repositories.AccountRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
-public class UserController implements UserApi {
+@RequestMapping("/api/v1/users")
+public class UserController {
 
     private final AccountRepository accountRepository;
 
@@ -21,9 +24,20 @@ public class UserController implements UserApi {
         this.accountRepository = accountRepository;
     }
 
-    @Override
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping
     public ResponseEntity<List<UserDto>> getUsers() {
         List<Account> accounts = accountRepository.findAll();
         return ResponseEntity.ok(UserMapper.INSTANCE.mapAll(accounts));
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Account currentUser = (Account) authentication.getPrincipal();
+
+        return ResponseEntity.ok(UserMapper.INSTANCE.map(currentUser));
+    }
+
 }
