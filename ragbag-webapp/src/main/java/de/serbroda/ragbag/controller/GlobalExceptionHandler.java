208f40c14405d4862b78135027d9a0ccce0f6c03
Danny Rottstegge <danny@rottstegge.net>
2024-09-06 12:57:37 +0200

    improve GlobalExceptionHandler

--- ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/GlobalExceptionHandler.java
+++ ragbag-webapp/src/main/java/de/serbroda/ragbag/controller/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package de.serbroda.ragbag.controller;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,66 +15,65 @@ import java.security.SignatureException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ProblemDetail handleUsernameNotFoundException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), "The username or password is incorrect");
-        errorDetail.setProperty("description", "The username or password is incorrect");
-        return errorDetail;
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
     public ProblemDetail handleBadCredentialsException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
-        errorDetail.setProperty("description", "The username or password is incorrect");
-        return errorDetail;
+        return createProblemDetails(
+                exception,
+                HttpStatus.UNAUTHORIZED,
+                "The username or password is incorrect"
+        );
     }
 
     @ExceptionHandler(AccountStatusException.class)
     public ProblemDetail handleAccountStatusException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-        errorDetail.setProperty("description", "The account is locked");
-        return errorDetail;
+        return createProblemDetails(
+                exception,
+                HttpStatus.FORBIDDEN,
+                "The account is locked"
+        );
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ProblemDetail handleAccessDeniedException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-        errorDetail.setProperty("description", "You are not authorized to access this resource");
-        return errorDetail;
+        return createProblemDetails(
+                exception,
+                HttpStatus.FORBIDDEN,
+                "You are not authorized to access this resource"
+        );
     }
 
     @ExceptionHandler(SignatureException.class)
     public ProblemDetail handleSignatureException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-        errorDetail.setProperty("description", "The JWT signature is invalid");
-        return errorDetail;
+        return createProblemDetails(
+                exception,
+                HttpStatus.FORBIDDEN,
+                "The JWT signature is invalid"
+        );
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ProblemDetail handleExpiredJwtException(Exception exception) {
-        exception.printStackTrace();
-
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-        errorDetail.setProperty("description", "The JWT token has expired");
-        return errorDetail;
+        return createProblemDetails(
+                exception,
+                HttpStatus.FORBIDDEN,
+                "The JWT token has expired"
+        );
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(Exception exception) {
+        return createProblemDetails(
+                exception,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Unknown internal server error"
+        );
+    }
+
+    private ProblemDetail createProblemDetails(Exception exception, HttpStatus status, String description) {
         exception.printStackTrace();
 
-        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
-        errorDetail.setProperty("description", "Unknown internal server error.");
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
+        errorDetail.setProperty("description", description);
         return errorDetail;
     }
 }
