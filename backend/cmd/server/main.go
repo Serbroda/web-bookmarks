b32dd5a62667d837f4b1d3f11999e8660253ff7e
Danny Rottstegge <danny@rottstegge.net>
2024-09-13 20:18:33 +0200

    tidy go mod

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -9,4 +10,14 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
+
+	printRoutes(e)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func printRoutes(e *echo.Echo) {
+	fmt.Printf("Registered following routes\n\n")
+	for _, r := range e.Routes() {
+		fmt.Printf(" - %v %v\n", r.Method, r.Path)
+	}
 }
